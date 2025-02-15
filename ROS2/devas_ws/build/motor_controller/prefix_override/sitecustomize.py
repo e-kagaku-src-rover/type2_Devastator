2954812b1bbfee79a83dd7e0f8f907095a838053
foqhn <21t2115k@shinshu-u.ac.jp>
2025-02-15 17:38:47 +0900

    delete ws

--- ROS2/devas_ws/build/motor_controller/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/tac/type2_Devastator/ROS2/devas_ws/install/motor_controller'
--- ROS2/devas_ws/build/motor_controller/share/motor_controller/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/tac/type2_Devastator/ROS2/devas_ws/build/motor_controller"
